bccd74de06faa93ac0d955db4fce8125c131c9e5
f.cavallo <>
2024-01-21 16:07:48 +0100

    added some bootstrap

--- work-days-fe/src/App.js
+++ work-days-fe/src/App.js
@@ -1,7 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
 import { useState } from "react";
-import { Collapsible } from "./components/Collapsible";
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const baseURL = 'http://localhost:3001'
 
@@ -35,20 +39,46 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Si Festa!</h1>
-      <header className="App-header">
-        <Collapsible />
-        <table>
-          <input type={'text'} name={"anno"} value={year} maxLength={4} onChange={handleChange}/>
-          {/* <button onClick={availableCountries}>availableCountries (first only)</button> */}
-          <button onClick={countryInfo}>countryInfo (Italy only)</button>
-          <button onClick={longWeekend}>longWeekend (2024 - Italy only)</button>
-          <button onClick={publicHolidays}>publicHolidays (2024 - Italy only)</button>
-          <button onClick={isTodayPublicHoliday}>isTodayPublicHoliday (Italy only)</button>
-          <button onClick={nextPublicHolidays}>nextPublicHolidays (Italy only)</button>
-          <button onClick={nextPublicHoliday}>nextPublicHoliday (Italy only)</button>
-        </table>
-      </header>
+        <Navbar expand="lg" className="bg-body-tertiary" sticky="top">
+          <Container>
+            <Navbar.Brand href="#home">Si Festa!</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#publicHolidays">Feste nazionali</Nav.Link>
+                <Nav.Link href="#longWeekends">Weekend lunghi</Nav.Link>
+                <NavDropdown title="Altre funzioni" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.2">
+                    Le prossime feste
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    È festa oggi?
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      <table>
+        <input type={'text'} name={"anno"} value={year} maxLength={4} onChange={handleChange}/>
+        {/* <button onClick={availableCountries}>availableCountries (first only)</button> */}
+        <button onClick={countryInfo}>countryInfo (Italy only)</button>
+        <button onClick={longWeekend}>longWeekend (Italy only)</button>
+        <button onClick={publicHolidays}>publicHolidays (2024 - Italy only)</button>
+        <button onClick={isTodayPublicHoliday}>isTodayPublicHoliday (Italy only)</button>
+        <button onClick={nextPublicHolidays}>nextPublicHolidays (Italy only)</button>
+        <button onClick={nextPublicHoliday}>nextPublicHoliday (Italy only)</button>
+      </table>
+      <Card>
+        <Card.Footer className="text-muted">
+          Powered by Francesco Cavallo (and some React)
+        </Card.Footer>
+      </Card>
     </div>
   );
 }
